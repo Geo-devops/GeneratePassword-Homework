837628f300a35318a5e73071c64f94af721825ab
GBondarek <georgettebondarek@gmail.com>
2020-10-01 20:39:31 -0400

    added var

--- script.js
+++ script.js
@@ -1,23 +1,25 @@
-// Assignment Code
+
+/*//*/// Assignment Code
 var generateBtn = document.querySelector("#generate");
 var copyBtn = document.querySelector("#copy");
+var passwordText = document.getElementById("#password");
 
-var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz";
-var numberCharacters = "0123456789";
-var specialCharacter = "!@#$%&*?";
+var upperCaseCharacters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    lowerCaseCharacters = "abcdefghijklmnopqrstuvwxyz",
+    numberCharacters = "0123456789",
+    specialCharacter = "!@#$%&*?",
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  var passwordLength = prompt("How many characters would you like your password to be? (Must be 8 or more)");
-  var lowerCaseCharacters = prompt("Would you like lowercase letters to be in your password?");
-  var upperCaseCharacters = prompt("Would you like Capital letters in your password?");
+    upperCaseCharacters = document.getElementById("upperCaseCharacters"),
+    lowerCaseCharacters = document.getElementById("lowerCaseCharacters"),
+    numberCharacters = document.getElementById("numberCharacters"),
+    specialCharacter = document.getElementById("specialCharacter");
 
-  passwordText.value = password;
 
+function generate() {
+  userPassword
 }
+// generate password with clidk 
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+
+// document.getElementById("buttonid").addEventListener("click", generatePassword);
+ 
